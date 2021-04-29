54d0354139cec381415cf73438781c6f829b3b69
Sam_Ash11414 <72979934+SamAsh11414@users.noreply.github.com>
2021-04-28 23:19:48 -0400

    login works

--- public/js/main.js
+++ public/js/main.js
@@ -1,57 +1,88 @@
 
-(function ($) {
-    "use strict";
+// (function ($) {
+//     "use strict";
 
     
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
+//     /*==================================================================
+//     [ Validate ]*/
+//     var input = $('.validate-input .input100');
 
-    $('.validate-form').on('submit',function(){
-        var check = true;
+//     $('.validate-form').on('submit',function(){
+//         var check = true;
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
+//         for(var i=0; i<input.length; i++) {
+//             if(validate(input[i]) == false){
+//                 showValidate(input[i]);
+//                 check=false;
+//             }
+//         }
 
-        return check;
-    });
+//         return check;
+//     });
 
 
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
+//     $('.validate-form .input100').each(function(){
+//         $(this).focus(function(){
+//            hideValidate(this);
+//         });
+//     });
 
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
+//     function validate (input) {
+//         if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+//             if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+//                 return false;
+//             }
+//         }
+//         else {
+//             if($(input).val().trim() == ''){
+//                 return false;
+//             }
+//         }
+//     }
 
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
+//     function showValidate(input) {
+//         var thisAlert = $(input).parent();
 
-        $(thisAlert).addClass('alert-validate');
-    }
+//         $(thisAlert).addClass('alert-validate');
+//     }
 
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
+//     function hideValidate(input) {
+//         var thisAlert = $(input).parent();
 
-        $(thisAlert).removeClass('alert-validate');
-    }
+//         $(thisAlert).removeClass('alert-validate');
+//     }
     
     
 
-})(jQuery);
+// })(jQuery);
+
+
+
+
+
+
+
+
+
+//   firebase.auth().signInWithEmailAndPassword(email, password)
+//   .then((userCredential) => {
+//     // Signed in
+//     var user = userCredential.user;
+//     // ...
+//   })
+//   .catch((error) => {
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//   });
+
+//   firebase.auth().onAuthStateChanged((user) => {
+//     if (user) {
+//       // User is signed in, see docs for a list of available properties
+//       // https://firebase.google.com/docs/reference/js/firebase.User
+//       var uid = user.uid;
+//       // ...
+//     } else {
+//       // User is signed out
+//       // ...
+//     }
+//   });
---
+++ public/js/signin.js
@@ -0,0 +1,37 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBTxAFBtZV3plHViPvJORHxs3YF2uOuPkI",
+    authDomain: "mathxportal.firebaseapp.com",
+    databaseURL: "https://mathxportal-default-rtdb.firebaseio.com",
+    projectId: "mathxportal",
+    storageBucket: "mathxportal.appspot.com",
+    messagingSenderId: "438926262255",
+    appId: "1:438926262255:web:08ab5e434d53a90b6c001a",
+    measurementId: "G-MN57T4VBQL"
+  };
+
+var app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth()
+var sign = document.getElementById("signIn")
+sign.addEventListener("click", signIn)
+
+function signIn(event){
+    event.preventDefault()
+    console.log("Signed In")
+    var email = document.getElementById("email")
+    var password = document.getElementById("password")
+    console.log(email.value, password.value)
+    
+    const promise = firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+    promise.catch(e => alert(e.message))
+    .then((userCredential) => {
+        var user = userCredential.user;
+        console.log(user)
+        var UUIDUSER = firebase.auth().currentUser.uid
+        console.log(UUIDUSER)
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
+}
