07dd34a3a582c6f6618d35d103d522582b73dc20
dv22ny <dv22ny@gmail.com>
2021-05-04 09:35:38 -0400

    updated frontend

--- public/js/ClassroomAccess.js
+++ public/js/ClassroomAccess.js
@@ -27,11 +27,11 @@ const classes = firebase.firestore().collection('SignedUpClasses').doc(UUID).get
     for (x in doc.data()) {
         var queryString = "class.html" + "?" + UUID + "?" + x + "?" + doc.data()[x];
         const li = `
-        <div style="border: 4px solid #0a84ff; background-color: #0a84ff; border-style: ridge; border-length: 20px; padding:10px; padding-bottom:20px; display: inline-grid; width: 31.2%; text-align: center; ">
+        <div style="border: 2px solid background-color #861657; background-image linear-gradient(326deg, #861657 0%, #ffa69e 74%); background-color: #a4508b; background-image: linear-gradient(326deg, #a4508b 0%, #5f0a87 74%); border-style: ridge; border-length: 20px; border-radius: 10px; padding:10px; padding-bottom:20px; display: inline-grid; width: 31.2%; text-align: center; ">
             <div >
                 <h2 style="color:#ffffff; padding-bottom: 20px; text-align: center;">${x}</h2>
                 <h3 style="color:#ffffff; text-align: center;">${doc.data()[x]}</h3>
-                <div style="text-align:center;"><button><a href="${queryString}"> Go To Class </a></button></div>
+                <div style="text-align:center;"><button style="background-color: #000000; border-radius: 10px; padding: 5px;"><a href="${queryString}" style="color: #630705; box-shadow:none; text-decoration:none; "> Go To Class </a></button></div>
             </div>
         </div>
         `;
