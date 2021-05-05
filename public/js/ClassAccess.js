d69adf13a629699df188e2b1181245f4f6bde5d7
Sam_Ash11414 <72979934+SamAsh11414@users.noreply.github.com>
2021-05-04 21:15:35 -0400

    Finished Stream

--- public/js/ClassAccess.js
+++ public/js/ClassAccess.js
@@ -1,9 +1,61 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBTxAFBtZV3plHViPvJORHxs3YF2uOuPkI",
+  authDomain: "mathxportal.firebaseapp.com",
+  databaseURL: "https://mathxportal-default-rtdb.firebaseio.com",
+  projectId: "mathxportal",
+  storageBucket: "mathxportal.appspot.com",
+  messagingSenderId: "438926262255",
+  appId: "1:438926262255:web:08ab5e434d53a90b6c001a",
+  measurementId: "G-MN57T4VBQL"
+};
+
 var queryString = decodeURIComponent(window.location.search)
+const classStream = document.getElementById('classMessages')
+
+var Submit = document.getElementById("Submission")
+Submit.addEventListener("click", SubmitStream)
+
+
+var app = firebase.initializeApp(firebaseConfig);
+
 console.log(queryString)
 queryString = queryString.substring(1);
 console.log(queryString)
 var queries = queryString.split("?");
-for (var i = 0; i < queries.length; i++)
-{
-  console.log(queries[i])
+// for (var i = 0; i < queries.length; i++)
+// {
+//   console.log(queries[i])
+// }
+
+function SubmitStream(event) {
+  event.preventDefault()
+  var TextSubmission = document.getElementById("TextSubmission")
+  var useremail = firebase.auth().currentUser.email
+  var today = new Date();
+  var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var SubmissionID = useremail + ' ' + date + ' ' + time;
+  var userRef = firebase.firestore().collection('Classes').doc(queries[1])
+
+  userRef.set({
+    [SubmissionID]: TextSubmission.value,
+  }, {merge: true})
 }
+
+const Stream = firebase.firestore().collection('Classes').doc(queries[1]).get().then(function(doc) {
+  console.log(doc.id, " => ", doc.data())
+  var x;
+  let StreamMessages = '';
+  for (x in doc.data()) {
+    const li = `
+    <div>
+      <h3>${x}</h3>
+      <h3>${doc.data()[x]}
+    </div>
+    `
+    StreamMessages += li;
+  }
+
+  classStream.innerHTML = StreamMessages
+
+})
