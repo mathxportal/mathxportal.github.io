220941e42093715eef0855d2aba93c5438d17783
Samuel Ashkenas <72979934+SamAsh11414@users.noreply.github.com>
2021-05-07 10:47:55 -0400

    REPLIES WORK

--- public/js/Reply.js
+++ public/js/Reply.js
@@ -8,9 +8,9 @@ var firebaseConfig = {
     appId: "1:438926262255:web:08ab5e434d53a90b6c001a",
     measurementId: "G-MN57T4VBQL"
   };
-
+var ReplyMessagesDisplay = document.getElementById("ReplyMessagesDisplay")
 var app = firebase.initializeApp(firebaseConfig);
-
+var replying = document.getElementById("Replying")
 var queryString = decodeURIComponent(window.location.search)
 queryString = queryString.substring(1);
 var queries = queryString.split("?");
@@ -23,14 +23,59 @@ var OriginalMessage = document.getElementById("OgMessage")
 const ContextMessage = `
 <div><p>${queries[2]}</p></div>
 <div><p>${queries[1]}</p></div>
-
 `
 
-function SubmitReply() {
+const ShowReplies = firebase.firestore().collection('Replies').doc(queries[0]).get().then(function(doc) {
+    console.log(doc.id, " => ", doc.data())
+    var x;
+    let ReplyMessages = '';
+    var MessagesArray = [];
+    for (x in doc.data()) {
+  
+      MessagesArray.push(x)
+      
+    }
+    MessagesArray.sort()
+      console.log(MessagesArray.reverse())
+      var y;
+      for (y in MessagesArray) {
+        console.log(MessagesArray[y])
+        var Text = MessagesArray[y].split('?')
+        const li = `
+        <div>
+          <p>${Text[0]}</p>
+          <p>${Text[1]}</p>
+        </div>
+        `
+        ReplyMessages += li;
+    }
+    ReplyMessagesDisplay.innerHTML = ReplyMessages
     
-}
+
+
+})
+
+ReplyText.addEventListener("click", SubmitReply)
+
+function SubmitReply(event) {
+    console.log(replying.value)
+    //event.preventDefault()
+    var today = new Date();
+    var Array = new Uint32Array(1);
+    window.crypto.getRandomValues(Array);
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var SubmissionID = date + ' ' + time +  ' ' + RespondersEmail;
+    var userRef = firebase.firestore().collection('Replies').doc(queries[0])
+  
+    userRef.set({
+      [SubmissionID + '?' + replying.value + '?' + Array]: replying.value,
+    }, {merge: true})
+    setTimeout(function(){
+        document.location.reload()
+    }, 2000)
+  }
 
 
 
 OriginalMessage.innerHTML = ContextMessage
-ReplyText.addEventListener("click", SubmitReply())
