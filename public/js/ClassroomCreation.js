b5e2447d1c234a6203fe537d329f80af294e8105
Samuel Ashkenas <72979934+SamAsh11414@users.noreply.github.com>
2021-05-19 18:46:05 -0400

    You can now create classes

--- public/js/ClassroomCreation.js
+++ public/js/ClassroomCreation.js
@@ -1,3 +1,29 @@
-var PlannedStart = document.getElementById('PlannedStart')
-var PlannedEnd = document.getElementById('PlannedEnd')
-var Description = document.getElementById('DescriptionOfCourse')
+var firebaseConfig = {
+    apiKey: "AIzaSyBTxAFBtZV3plHViPvJORHxs3YF2uOuPkI",
+    authDomain: "mathxportal.firebaseapp.com",
+    databaseURL: "https://mathxportal-default-rtdb.firebaseio.com",
+    projectId: "mathxportal",
+    storageBucket: "mathxportal.appspot.com",
+    messagingSenderId: "438926262255",
+    appId: "1:438926262255:web:08ab5e434d53a90b6c001a",
+    measurementId: "G-MN57T4VBQL"
+  };
+
+var app = firebase.initializeApp(firebaseConfig);
+
+// var CourseName = document.getElementById('CourseName')
+// var Description = document.getElementById('DescriptionOfCourse')
+// var PlannedStart = document.getElementById('PlannedStart')
+// var PlannedEnd = document.getElementById('PlannedEnd')
+
+document.getElementById("SubmitClass").addEventListener('click', function(){
+    var CourseName = document.getElementById('CourseName')
+    var Description = document.getElementById('DescriptionOfCourse')
+    var PlannedStart = document.getElementById('PlannedStart')
+    var PlannedEnd = document.getElementById('PlannedEnd')
+    const SetCourseName = firebase.firestore().collection('Classes').doc(CourseName.value)
+    const SetCourseSettings = firebase.firestore().collection('ClassID').doc(CourseName.value).set({
+        Description: [Description.innerHTML],
+        PlannedStart: [PlannedStart.value],
+    })
+})
