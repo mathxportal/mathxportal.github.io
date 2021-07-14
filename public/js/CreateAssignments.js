42e1c88a75e048aef254da395407ae7789463b77
Samuel Ashkenas <72979934+SamAsh11414@users.noreply.github.com>
2021-07-14 19:23:00 -0400

    Jitsi added

--- public/js/CreateAssignments.js
+++ public/js/CreateAssignments.js
@@ -1,71 +1,83 @@
-var quill = new Quill('#editor-container', {
-    modules: {
-      toolbar: [
-        [{ header: [1, 2, 3, 4, 5, 6,  false] }],
-        ['bold', 'italic', 'underline','strike'],
-        ['image', 'code-block'],
-        ['link'],
-        [{ 'script': 'sub'}, { 'script': 'super' }],
-        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-        ['clean']
-      ]
-    },
-    placeholder: 'Compose an epic...',
-    theme: 'snow'  // or 'bubble'
-  });
-
+var quill = new Quill("#editor-container", {
+  modules: {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ["bold", "italic", "underline", "strike"],
+      ["image", "code-block"],
+      ["link"],
+      [{ script: "sub" }, { script: "super" }],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ["clean"],
+    ],
+  },
+  placeholder: "Compose an epic...",
+  theme: "snow", // or 'bubble'
+});
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBTxAFBtZV3plHViPvJORHxs3YF2uOuPkI",
-    authDomain: "mathxportal.firebaseapp.com",
-    databaseURL: "https://mathxportal-default-rtdb.firebaseio.com",
-    projectId: "mathxportal",
-    storageBucket: "mathxportal.appspot.com",
-    messagingSenderId: "438926262255",
-    appId: "1:438926262255:web:08ab5e434d53a90b6c001a",
-    measurementId: "G-MN57T4VBQL"
-  };
+  apiKey: "AIzaSyBTxAFBtZV3plHViPvJORHxs3YF2uOuPkI",
+  authDomain: "mathxportal.firebaseapp.com",
+  databaseURL: "https://mathxportal-default-rtdb.firebaseio.com",
+  projectId: "mathxportal",
+  storageBucket: "mathxportal.appspot.com",
+  messagingSenderId: "438926262255",
+  appId: "1:438926262255:web:08ab5e434d53a90b6c001a",
+  measurementId: "G-MN57T4VBQL",
+};
 
 var app = firebase.initializeApp(firebaseConfig);
 
-var ClassName = document.getElementById("Classname")
-var queryString = decodeURIComponent(window.location.search)
+var ClassName = document.getElementById("Classname");
+var queryString = decodeURIComponent(window.location.search);
 queryString = queryString.substring(1);
-console.log(queryString)
+console.log(queryString);
 
-ClassName.innerText += " " + queryString
+ClassName.innerText += " " + queryString;
 
-
-firebase.auth().onAuthStateChanged(async function(user) {
-    let UUID = firebase.auth().currentUser.uid
-    firebase.firestore().collection("SignedUpClasses").doc(UUID).get().then(function(doc){
-        let i = 0
-        for (var x in doc.data()){
-            if (x == queryString && doc.data()[x] == "Teacher") {
-                i = 2
-            }
+firebase.auth().onAuthStateChanged(async function (user) {
+  let UUID = firebase.auth().currentUser.uid;
+  firebase
+    .firestore()
+    .collection("SignedUpClasses")
+    .doc(UUID)
+    .get()
+    .then(function (doc) {
+      let i = 0;
+      for (var x in doc.data()) {
+        if (x == queryString && doc.data()[x] == "Teacher") {
+          i = 2;
         }
+      }
 
-        if (i > 1) {
-            document.getElementById('SubmitAssignment').addEventListener('click', function(){
-                var Name = document.getElementById("AssignmentName").value
-                var Description = quill.container.innerHTML
-                var DueDate = document.getElementById('DueDate')
-                console.log(Name + Description.value + DueDate.value)
-                const AssignmentLink = firebase.firestore().collection("ClassID").doc(queryString).collection("Assignments").doc(Name).set({
-                    assignmentName: [Name],
-                    assignmentDescription: [Description],
-                    dueDate: [DueDate.value]
-                }).then(function(){
-                    location.replace("author-dashboard.html")
-                })
-            })
-        }
-
-        else {
-            document.getElementById('SubmitAssignment').addEventListener('click', function(){
-                alert("You are not a teacher")
-            })
-        }
-    })
-})
+      if (i > 1) {
+        document
+          .getElementById("SubmitAssignment")
+          .addEventListener("click", function () {
+            var Name = document.getElementById("AssignmentName").value;
+            var Description = quill.container.innerHTML.split("<div class=\"ql-tooltip ql-hidden\" style=\"user-select: auto;\">")[0];
+            var DueDate = document.getElementById("DueDate");
+            console.log(Name + Description.value + DueDate.value);
+            const AssignmentLink = firebase
+              .firestore()
+              .collection("ClassID")
+              .doc(queryString)
+              .collection("Assignments")
+              .doc(Name)
+              .set({
+                assignmentName: [Name],
+                assignmentDescription: [Description],
+                dueDate: [DueDate.value],
+              })
+              .then(function () {
+                location.replace("author-dashboard.html");
+              });
+          });
+      } else {
+        document
+          .getElementById("SubmitAssignment")
+          .addEventListener("click", function () {
+            alert("You are not a teacher");
+          });
+      }
+    });
+});
---
+++ public/js/Jitsi.js
@@ -0,0 +1,14 @@
+var queryString = decodeURIComponent(window.location.search)
+console.log(queryString)
+queryString = queryString.substring(1);
+console.log(queryString)
+var queries = queryString.split("?");
+
+const domain = 'meet.jit.si';
+const options = {
+    roomName: queries[1],
+    width: 700,
+    height: 700,
+    parentNode: document.querySelector('#meet')
+};
+const api = new JitsiMeetExternalAPI(domain, options);
